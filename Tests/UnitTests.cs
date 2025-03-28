29a24075005226c4dc3c337a1cd5ade6921d30b5
noahr <noahr@NOAH-LAPTOP>
2025-03-28 11:41:44 -0600

    added name to tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Patino_Russell()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Patino_Russell()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,7 +20,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Patino_Russell()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -32,7 +32,7 @@ public class Addition
 public class Subtraction
 {
     [TestMethod]
-    public void Subtract_Valid_Patino()
+    public void Subtract_Valid_Patino_Russell()
     {
         Assert.AreEqual(1, Program.Subtract("2", "1"));
         Assert.AreEqual(1, Program.Subtract("3", "2"));
@@ -40,7 +40,7 @@ public class Subtraction
     }
 
     [TestMethod]
-    public void Subtract_Invalid_Patino()
+    public void Subtract_Invalid_Patino_Russell()
     {
         Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
@@ -48,7 +48,7 @@ public class Subtraction
     }
 
     [TestMethod]
-    public void Subtract_Null_Patino()
+    public void Subtract_Null_Patino_Russell()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -61,7 +61,7 @@ public class Subtraction
 public class Division
 {
     [TestMethod]
-    public void Divide_Valid_Patino()
+    public void Divide_Valid_Patino_Russell()
     {
         Assert.AreEqual(2, Program.Divide("2", "1"));
         Assert.AreEqual(1, Program.Divide("3", "3"));
@@ -69,7 +69,7 @@ public class Division
     }
 
     [TestMethod]
-    public void Divide_Invalid_Patino()
+    public void Divide_Invalid_Patino_Russell()
     {
         Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
@@ -77,7 +77,7 @@ public class Division
     }
 
     [TestMethod]
-    public void Divide_Null_Patino()
+    public void Divide_Null_Patino_Russell()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -89,7 +89,7 @@ public class Division
 public class Multiplication
 {
     [TestMethod]
-    public void Multiply_Valid_Patino()
+    public void Multiply_Valid_Patino_Russell()
     {
         Assert.AreEqual(2, Program.Multiply("2", "1"));
         Assert.AreEqual(9, Program.Multiply("3", "3"));
@@ -97,7 +97,7 @@ public class Multiplication
     }
 
     [TestMethod]
-    public void Multiply_Invalid_Patino()
+    public void Multiply_Invalid_Patino_Russell()
     {
         Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
@@ -105,7 +105,7 @@ public class Multiplication
     }
 
     [TestMethod]
-    public void Multiply_Null_Patino()
+    public void Multiply_Null_Patino_Russell()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -117,7 +117,7 @@ public class Multiplication
 public class Power
 {
     [TestMethod]
-    public void Power_Valid_Patino()
+    public void Power_Valid_Patino_Russell()
     {
         Assert.AreEqual(2, Program.Power("2", "1"));
         Assert.AreEqual(27, Program.Power("3", "3"));
@@ -125,7 +125,7 @@ public class Power
     }
 
     [TestMethod]
-    public void Power_Invalid_Patino()
+    public void Power_Invalid_Patino_Russell()
     {
         Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
         Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
@@ -133,7 +133,7 @@ public class Power
     }
 
     [TestMethod]
-    public void Power_Null_Patino()
+    public void Power_Null_Patino_Russell()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
